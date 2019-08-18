ee063a202718e1fd56d32f9e342b3a94f75bea66
Armin Blaze <box_for_spam@mail.ru>
2019-08-18 17:57:46 +0300

    8 - Reverse String (All Solutions)

--- Udemy-Interview/exercises/8-reversestring/index.js
+++ Udemy-Interview/exercises/8-reversestring/index.js
@@ -7,14 +7,32 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-//	return str.split('').reverse().join('');
-	
-	var result = '';
-	var length = str.length-1;
-	for(var i = length; i >= 0; i--) {
-		result+=str[i];
-	}
-	return result;
+	return str.split('').reduce( (result, currentChar) => currentChar + result,
+															"")
 }
+
+
+//function reverse(str) {
+//	var result = '';
+//	for(var char of str) {
+//		result = char + result;
+//	}
+//	return result;
+//}
+
+//function reverse(str) {
+//	var result = '';
+//	var length = str.length-1;
+//	for(var i = length; i >= 0; i--) {
+//		result+=str[i];
+//	}
+//	return result;
+//}
+
+//function reverse(str) {
+//	return str.split('').reverse().join('');
+//}
+
+
 
 module.exports = reverse;

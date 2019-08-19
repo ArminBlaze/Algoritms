1e7bcd305b3b653a5c11899364c4f706d3deee82
Armin Blaze <box_for_spam@mail.ru>
2019-08-20 01:54:56 +0300

    5 - Reverse Int (-51 >>> -15)

--- Udemy-Interview/exercises/5-reverseint/index.js
+++ Udemy-Interview/exercises/5-reverseint/index.js
@@ -8,6 +8,27 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+
+function reverseInt(n) {
+	var sign = Math.sign(n);
+
+	var reversed = n.toString().split('').reverse().join('');
+	return sign * parseInt(reversed);
+}
+
+//function reverseInt(n) {
+//	var sign = Math.sign(n);
+//	
+//	var strInt = n*sign + '';
+//	return sign * strInt.split('').reverse().join('');
+//}
+
+//function reverseInt(n) {
+//	var strInt = n + '';
+//	if(n<0) {
+//		return -strInt.split('-')[1].split('').reverse().join('');
+//	}
+//	return +strInt.split('').reverse().join('')
+//}
 
 module.exports = reverseInt;

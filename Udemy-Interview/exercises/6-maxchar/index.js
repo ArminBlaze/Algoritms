9f20065a5a3517e8465ffaa55c8ba737aa1ca07d
Armin Blaze <box_for_spam@mail.ru>
2019-08-20 19:27:49 +0300

    6.21 - Max Chars (all solutions)

--- Udemy-Interview/exercises/6-maxchar/index.js
+++ Udemy-Interview/exercises/6-maxchar/index.js
@@ -5,26 +5,77 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
+//авторское решение с 2мя проходами
 function maxChar(str) {
-	var counterObj = {};
+	var charMap = {};
+	var max = 0;
+	let maxLetter = '';
 	
 	//считаем вхождения в аналог map
 	for(var char of str) {
-		if(counterObj[char] === undefined) {
-			counterObj[char] = 1;
+		if(!charMap[char]) {
+			charMap[char] = 1;
 		}
 		else {
-			counterObj[char] += 1;
+			charMap[char]++;
 		}
 	}
 	
-	//сортируем массив по количеству повторений
-	var counterArr = Object.entries(counterObj).sort((a, b) => {
-		a[1] - b[1]
-	});
-	
+	for (let char in charMap) {
+		if(charMap[char] > max) {
+			max = charMap[char];
+			maxLetter = char;
+		}
+	}
+
 	//возвращаем самую частую букву в слове
-	return counterArr[0][0];
+	return maxLetter;
 }
+
+
+
+//решение с одним проходом по строке
+//function maxChar(str) {
+//	var charMap = {};
+//	var max = 0;
+//	let maxLetter = str[0];
+//	
+//	//считаем вхождения в аналог map
+//	for(var char of str) {
+//		if(!charMap[char]) {
+//			charMap[char] = 1;
+//		}
+//		else {
+//			var value = charMap[char] + 1;
+//			charMap[char] = value;
+//			if(value > max) {
+//				max = value;
+//				maxLetter = char;
+//			}
+//		}
+//	}
+//
+//	//возвращаем самую частую букву в слове
+//	return maxLetter;
+//}
+
+
+
+//function maxChar(str) {
+//	var charMap = {};
+//	
+//	//считаем вхождения в аналог map
+//	for(var char of str) {
+//		charMap[char] = charMap[char] + 1 || 1;
+//	}
+//	
+//	//сортируем массив по количеству повторений
+//	var counterArr = Object.entries(charMap).sort((a, b) => {
+//		a[1] - b[1]
+//	});
+//	
+//	//возвращаем самую частую букву в слове
+//	return counterArr[0][0];
+//}
 
 module.exports = maxChar;
